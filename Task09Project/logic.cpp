2674a4d7d9e8f2376f8e89cad0f9dda9f53447fd
KrutalevichLev <krutaleviclev@gmail.com>
2026-04-08 16:59:51 +0300

    Add project files.

---
+++ Task09Project/logic.cpp
@@ -0,0 +1,27 @@
+﻿// Task 09 [The sum VI]
+// Сумма
+//
+// Необходимо спроектировать и реализовать программу, 
+// которая вычисляет сумму цифр заданного числа, кратных 5.
+
+#include "logic.h"
+
+int recursion(int number) {
+    if (number == 0) {
+        return 0;
+    }
+
+    int last_digit = number % 10;
+
+
+    int add = last_digit % 5 != 0 ? 0 : last_digit;
+
+    return add + sum(number / 10);
+}
+
+int sum(int number) {
+
+    number = (number < 0) ? -number : number;
+
+    return recursion(number);
+}
